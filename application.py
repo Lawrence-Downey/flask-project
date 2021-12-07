0b491ff8500d6e10e1b9e55651479ca3f6203553
Lawrence.Downey <cooperandalex@outlook.com>
2021-12-06 23:43:26 -0330

    Second commit

--- application.py
+++ application.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, redirect, request, abort
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
+@app.route("/Home")
+def home():
     return render_template("Home.html")
 
+@app.route("/collection")
+def collection():
+    return render_template("collection.html")
 
