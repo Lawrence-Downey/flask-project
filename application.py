7828c6674db400c9a0e43b115cd74ca3b0b86067
Lawrence.Downey <cooperandalex@outlook.com>
2021-12-08 01:25:47 -0330

    Set up Collection, Ad and Remove Movie pages while also getting videos and images to work via Jinja2.

--- application.py
+++ application.py
@@ -1,14 +1,29 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
-@app.route("/Home")
+@app.route("/")
 def home():
-    return render_template("Home.html")
+    imageFile = url_for('static', filename='/nowPlaying.jpg')
+    return render_template("Home.html", imageFile=imageFile)
+
 
 @app.route("/collection")
 def collection():
-    return render_template("collection.html")
+    #toyStoryPoster = url_for('static', filename='static/ToyStoryPoster.jpg')
+    tmntPoster = url_for('static', filename='/teenageMutantNinjaTurtlesPoster.jpg')
+    grinchPoster = url_for('static', filename='/howTheGrinchStoleChristmasPoster.jpg')
+    homeAlonePoster = url_for('static', filename='/homeAlonePoster.jpg')
+    return render_template("collection.html", tmntPoster=tmntPoster,
+                           grinchPoster=grinchPoster, homeAlonePoster=homeAlonePoster)
 
+
+@app.route("/addMovie")
+def addMovie():
+    return render_template("addMovie.html")
+
+
+@app.route("/removeMovie")
+def removeMovie():
+    return render_template("removeMovie.html")
