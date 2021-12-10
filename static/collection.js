33f0630491bbaa399999b1c97e7f9ea3603bdac1
Lawrence.Downey <cooperandalex@outlook.com>
2021-12-09 23:29:44 -0330

    Changed the functionality to better suit the app.

--- static/collection.js
+++ static/collection.js
@@ -2,14 +2,14 @@ $(document).ready( () => {
 
     $("#collection a").click(function(evt) {
         if($(this).next().attr("class") != "hide"){
-            $(this).next().hide().toggleClass("hide").animate({opacity: 0.1})
+            $(this).next().fadeOut(1500).toggleClass("hide");
         }else{
-            $(this).next().show().toggleClass("hide").animate({opacity:1});
+            $(this).next().fadeIn(1000).toggleClass("hide");
         };
         evt.preventDefault();        
     });
 
     setTimeout(function(){
     $("#centerImage").fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500)
-    })
+    }, 2000);
 });
