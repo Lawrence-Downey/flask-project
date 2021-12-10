0369eb66bc7493b89273884d1b86fc507864f4c3
Lawrence.Downey <cooperandalex@outlook.com>
2021-12-09 23:26:50 -0330

    Adding functionality for accessing the databas for Add and Remove Movies.

--- application.py
+++ application.py
@@ -6,7 +6,8 @@ from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-
+conn = sqlite3.connect("movies.db")
+conn.row_factory = sqlite3.Row
 
 
 
@@ -30,33 +31,50 @@ def home():
 
 
 @app.route("/collection")
-def collection():
-    conn = sqlite3.connect("movies.db")
-    conn.row_factory = sqlite3.Row
+def collection(conn):
     with closing(conn.cursor()) as c:
-            query = '''SELECT * FROM movies'''
-            c.execute(query)
-            results = c.fetchall()
+        query = '''SELECT * FROM movies'''
+        c.execute(query)
+        results = c.fetchall()
     listOfMovies = []
     for row in results:
         listOfMovies.append(row)
+    if conn:
+        conn.close()
     width = "500"
     height = "700"
+
     return render_template("collection.html", listOfMovies=listOfMovies, width=width, height=height)
 
 
-@app.route("/addMovie")
-def addMovie():
+@app.route("/addMovie", methods=["GET", "POST"])
+def addMovie(conn):
+    #if request.method == "POST":
+    filename = request.form.get("filename")
+    movieTitle = request.form.get("movieTitle")
+    runtime = request.form.get("runtime")
+    starring = request.form.get("starring")
+    genre = request.form.get("genre")
+    boxOffice = request.form.get("boxOffice")
+
+    with closing(conn.cursor()) as c:
+        query = '''INSERT INTO movies (filename, movieTitle, runtime,
+                                        starring, genre, boxOffice)
+                    VALUES (?, ?, ?, ?, ?, ?)'''
+        c.execute(query, (filename, movieTitle, runtime, starring, genre, boxOffice))
+        conn.commit()
     return render_template("addMovie.html")
 
 
 @app.route("/removeMovie")
 def removeMovie():
-    return render_template("removeMovie.html")
+    removeImage = url_for('static', filename='/removeMovieImage.jpg')
+    width = "450"
+    height = "450"
+    return render_template("removeMovie.html", removeImage=removeImage, width=width, height=height)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     data = request.form
-    print(data)
     return render_template("login.html", boolean=True)
